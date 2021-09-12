ff194ca084a2b017a96d7c24a0257d0556a35393
Richard Quinlivan <richardquinlivan4444@gmail.com>
2021-09-12 18:07:43 -0400

    did some code reformating on the files and added the empty and type_name functions for int

--- Deque.hpp
+++ Deque.hpp
@@ -1,44 +1,58 @@
 #ifndef DEQUE_H
 #define DEQUE_H
 
+#include <assert.h>
+#include <string.h>
 
-#define Deque_DEFINE(t)																	\
-struct Deque_##t																		\
-{																						\
-																						\
-																						\
-																						\
-}																						\
-																						\
-struct Iterator_##t																		\
-{																						\
-																						\
-																						\
-																						\
-}																						\
 
-typedef struct Deque 
+#define Deque_DEFINE(t)														\
+struct Deque_##t															\
+{																			\
+																			\
+																			\
+																			\
+};																			\
+																			\
+struct Iterator_##t															\
+{																			\
+																			\
+																			\
+																			\
+};																			\
+
+typedef struct Deque
 {
 	int ** data;
-	size_t size;
-	size_t &(*size)(struct Deque *dp);
-	int &(*at)(struct Deque *dp, int i);
+	size_t length;
+	size_t (*size)(struct Deque *dp);
+	bool (*empty)(struct Deque *dp);
+	int (*at)(struct Deque *dp, int i);
 
 
 } Deque;
 
-size_t Deque_int_size(struct Deque *dp)
+char[] Deque_type_name(struct Deque *dp)
 {
-	return dp->size;
+	int length = 7 + strlen("int"); //strlen(#t) //should get the string version of the type name for the macro version
 }
 
-int Array_int_at(struct Deque *dp, int i) 
+bool Deque_empty(struct Deque *dp)
 {
-	assert(i < size);
-	return ap->data[i];
-} 
+	return dp->size(dp) == 0;
+}
 
-typedef struct Iterator 
+size_t Deque_int_size(struct Deque *dp)
+{
+	return dp->length;
+}
+
+int Deque_int_at(struct Deque *dp, int i)
+{
+	assert(i < dp->length && i >= 0);
+	return dp->data[i];
+}
+
+typedef struct Iterator
 {
 
 
